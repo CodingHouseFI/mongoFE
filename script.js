313890b73f0bd5a597b607ca84dc57baa8cd2650
AgileLabs <samer@agilelabs.com>
2015-07-02 16:59:10 -0700

    Ask question

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 'use strict';
 
 angular.module('heimdall', [])
+.constant("ATN", {
+  "API_URL": "http://localhost:3000"
+})
+.factory('Question', function($http, ATN) {
+  return {
+    getAll: function() {
+      return $http.get(ATN.API_URL + "/questions");
+    },
 
-.controller('MainCtrl', function($scope, $http){
-  $http.get("http://localhost:3000/questions").success(function(data) {
+    addQuestion: function(newQuestion) {
+      return $http.post(ATN.API_URL + "/questions", newQuestion);
+    }
+  }
+})
+.controller('MainCtrl', function($scope, Question){
+  Question.getAll().success(function(data) {
     $scope.questions = data;
-  }).error(function(err) {
-    console.log(err);
+  }).catch(function(err) {
+    console.error(err);
   });
+
+  $scope.askQuestion = function() {
+    Question.addQuestion($scope.question)
+      .success(function(data) {
+        $scope.questions.unshift(data);
+        $scope.question = {};
+        $("#new-question-modal").modal("hide");
+      })
+      .catch(function(err) {
+        console.error(err);
+      })
+  };
+
 });
